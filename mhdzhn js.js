fbeb764cdf19bc2bea8a140efe70abf75371967a
mhdzhn <134906204+mhdzhn@users.noreply.github.com>
2024-11-23 11:50:18 +0800

    Add files via upload

---
+++ mhdzhn js.js
@@ -0,0 +1,5 @@
+// A simple script for demonstration
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website loaded successfully!");
+    alert("Welcome to My Website!");
+});
